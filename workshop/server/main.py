8fc44d06692c59be6dcbe0f65a1ba20f977610f7
Michael Niepel <michael.niepel@exxeta.com>
2022-09-06 14:34:04 +0200

    remove sync code

--- workshop/server/main.py
+++ workshop/server/main.py
@@ -1,44 +1,151 @@
-import paho.mqtt.client as paho
-from paho import mqtt
+import asyncio
+from contextlib import AsyncExitStack
+import json
+from asyncio_mqtt import Client, MqttError
+from contextlib import suppress
+from racetrack import Racetrack, TrackState
 
-# setting callbacks for different events to see if it works, print the message etc.
-def on_connect(client, userdata, flags, rc, properties=None):
-    print(f"CONNACK received with code {rc}.")
+# pub
+SPEED_TOPIC = "speed"
+LIGHTS_TOPIC = "lights"
 
-# with this callback you can see if your publish was successful
-def on_publish(client, userdata, mid, properties=None):
-    print(f"mid: {str(mid)}")
+# sub
+DISTANCE_TOPIC = "distance"
+LAP_TOPIC = "lap"
+OVERHEAT_TOPIC = "overheat"
+BUTTON_TOPIC = "button"
 
-# print which topic was subscribed to
-def on_subscribe(client, userdata, mid, granted_qos, properties=None):
-    print(f"Subscribed: {str(mid)} {str(granted_qos)}")
+TOPIC_ROOT = "racetrack"
+LOG_FORMAT = "Received: {}"
 
-# print message, useful for checking if it was successful
-def on_message(client, userdata, msg):
-    print(f"{msg.topic} {str(msg.qos)} {str(msg.payload)}")
+# racetrack state
+RACETRACK = None
 
-# MQTTv31 for myqtthub
-client = paho.Client(client_id="api_server", userdata=None, protocol=paho.MQTTv31)
-client.on_connect = on_connect
 
-# enable TLS for secure connection
-client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
-# set username and password
-client.username_pw_set("api_server", "PyConSK2022")
+async def racetrack():
+    # Context manager
+    async with AsyncExitStack() as stack:
+        tasks = set()
+        stack.push_async_callback(cancel_tasks, tasks)
 
-client.connect("node02.myqtthub.com", 8883)
+        # Connect to the MQTT broker
+        client = Client("node02.myqtthub.com",
+                        client_id="api_server",
+                        username="api_server",
+                        password="PyConSK2022")
+        await stack.enter_async_context(client)
 
-# setting callbacks, use separate functions like above for better visibility
-client.on_subscribe = on_subscribe
-client.on_message = on_message
-client.on_publish = on_publish
+        sub_to = [DISTANCE_TOPIC, LAP_TOPIC, OVERHEAT_TOPIC, BUTTON_TOPIC]
+        topic_filters = [(f"{TOPIC_ROOT}/{topic}", topic) for topic in sub_to]
+        for topic_filter, topic in topic_filters:
+            manager = client.filtered_messages(topic_filter)
+            messages = await stack.enter_async_context(manager)
+            task = asyncio.create_task(
+                on_message_received(client, messages, topic))
+            tasks.add(task)
 
-# subscribe to all topics
-client.subscribe("esp_1/#", qos=1)
+        # Subscribe to everything in racetrack/...
+        await client.subscribe(f"{TOPIC_ROOT}/#")
 
-# a single publish
-client.publish("esp_1/led", payload="ON", qos=1)
+        await init_track(client)
 
-# loop_forever for simplicity, here you need to stop the loop manually
-# you can also use loop_start and loop_stop
-client.loop_forever()
+        # Wait for everything to complete (or fail due to, e.g., network
+        # errors)
+        await asyncio.gather(*tasks)
+
+
+async def init_track(client):
+    global RACETRACK
+    RACETRACK = Racetrack()
+    # stop tracks
+    msg = {"track": 0, "speed": 0}
+    await publish_speed(client, msg)
+    msg = {"track": 1, "speed": 0}
+    await publish_speed(client, msg)
+    await publish_lights(client, {"command": "blink_red"})
+
+async def start_race(client, time):
+    await publish_lights(client, {"command": "rsg"})
+    await asyncio.sleep(3)
+    RACETRACK.start_race(time)
+    msg = {"track": 0, "speed": Racetrack.INITIAL_SPEED}
+    await publish_speed(client, msg)
+    msg = {"track": 1, "speed": Racetrack.INITIAL_SPEED}
+    await publish_speed(client, msg)
+
+async def stop_race(client):
+    RACETRACK.stop_race()
+    await publish_lights(client, {"command": "blink_red"})
+    msg = {"track": 0, "speed": 0}
+    await publish_speed(client, msg)
+    msg = {"track": 1, "speed": 0}
+    await publish_speed(client, msg)
+
+async def publish_speed(client, message: dict):
+    print(f"Publish: {message}")
+    await client.publish(f"{TOPIC_ROOT}/{SPEED_TOPIC}",
+                         json.dumps(message),
+                         qos=1)
+
+
+async def publish_lights(client, message: dict):
+    print(f"Publish: {message}")
+    await client.publish(f"{TOPIC_ROOT}/{LIGHTS_TOPIC}",
+                         json.dumps(message),
+                         qos=1)
+
+
+async def on_message_received(client, messages, topic):
+    global RACETRACK
+    # based on topics
+    async for message in messages:
+        print(LOG_FORMAT.format(message.payload.decode()))
+        decoded_message = json.loads(message.payload.decode())
+        # control speed
+        if topic == DISTANCE_TOPIC:
+            if RACETRACK.STATE == TrackState.RUNNING:
+                speed_message = {
+                    "track": decoded_message.get("track"),
+                    "speed": Racetrack.speed_from_distance(decoded_message.get("distance"))
+                }
+                await publish_speed(client, speed_message)
+        elif topic == BUTTON_TOPIC:
+            time = decoded_message.get("time")
+            if RACETRACK.STATE == TrackState.IDLE:
+                await start_race(client, time)
+            # publish initial speeds
+            elif RACETRACK.STATE == TrackState.RUNNING:
+                await stop_race(client)
+        elif topic == LAP_TOPIC:
+            if RACETRACK.STATE == TrackState.RUNNING:
+                track_id = RACETRACK.lap(decoded_message.get("track"))
+                if  track_id != -1:
+                    print(f">>>> {track_id} wins!")
+                    await stop_race(client)
+
+
+async def cancel_tasks(tasks):
+    for task in tasks:
+        if task.done():
+            continue
+        with suppress(asyncio.CancelledError):
+            task.cancel()
+            await task
+
+
+async def main():
+    # Run the racetrack indefinitely. Reconnect automatically
+    # if the connection is lost.
+    reconnect_interval = 3  # [seconds]
+    while True:
+        try:
+            await racetrack()
+        except MqttError as error:
+            print(
+                f'Error "{error}". Reconnecting in {reconnect_interval} seconds.'
+            )
+        finally:
+            await asyncio.sleep(reconnect_interval)
+
+
+asyncio.run(main())
